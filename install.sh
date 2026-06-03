d2afb0c45ddaca3b61b81bd325836485e3437326
Vincent Jadraque <vincent.jadraque@gmail.com>
2026-06-03 14:47:07 +1000

    feat: rename and store aliases in ~/.oh-my-zsh/custom/

--- install.sh
+++ install.sh
@@ -7,13 +7,15 @@ VSCODE="$HOME/Library/Application Support/Code/User"
 FILES=(
   ".gitconfig"
   ".gitignore_global"
-  ".zsh_aliases"
 )
 
 for f in "${FILES[@]}"; do
   [ -f "$HOME/$f" ] && mv "$HOME/$f" "$HOME/$f.bak"
   ln -sf "$DOTFILES/$f" "$HOME/$f"
 done
+
+[ -f "$HOME/.oh-my-zsh/custom/aliases.zsh" ] && mv "$HOME/.oh-my-zsh/custom/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh.bak"
+ln -sf "$DOTFILES/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh"
 
 [ -f "$VSCODE/settings.json" ] && mv "$VSCODE/settings.json" "$VSCODE/settings.json.bak"
 ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE/settings.json"
