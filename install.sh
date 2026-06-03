ef98e0b1ff06aa5fc228e1679222065db519127d
Vincent Jadraque <vincent.jadraque@gmail.com>
2026-06-03 15:03:02 +1000

    refactor: installation backup and link into a function

--- install.sh
+++ install.sh
@@ -4,20 +4,25 @@ set -e
 DOTFILES="$HOME/dotfiles"
 VSCODE="$HOME/Library/Application Support/Code/User"
 
+# Back up an existing file/symlink to <path>.bak, then symlink it to the source.
+backup_and_link() {
+  local src="$1" dest="$2"
+  if [ -e "$dest" ] || [ -L "$dest" ]; then
+    mv "$dest" "$dest.bak"
+  fi
+  ln -sf "$src" "$dest"
+}
+
 FILES=(
   ".gitconfig"
   ".gitignore_global"
 )
 
 for f in "${FILES[@]}"; do
-  [ -f "$HOME/$f" ] && mv "$HOME/$f" "$HOME/$f.bak"
-  ln -sf "$DOTFILES/$f" "$HOME/$f"
+  backup_and_link "$DOTFILES/$f" "$HOME/$f"
 done
 
-[ -f "$HOME/.oh-my-zsh/custom/aliases.zsh" ] && mv "$HOME/.oh-my-zsh/custom/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh.bak"
-ln -sf "$DOTFILES/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh"
-
-[ -f "$VSCODE/settings.json" ] && mv "$VSCODE/settings.json" "$VSCODE/settings.json.bak"
-ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE/settings.json"
+backup_and_link "$DOTFILES/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh"
+backup_and_link "$DOTFILES/vscode/settings.json" "$VSCODE/settings.json"
 
 echo "Done. Existing files backed up with .bak extension."
